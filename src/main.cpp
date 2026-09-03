9c7913f78a7d07a920e71d6e443bb563e47dc1a4
Tajul Tonim <tajul.islam.tonim@gmail.com>
2026-09-04 01:04:41 +0600

    Refactor main function to accept file input and enhance usage instructions

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,66 @@
+constexpr const char* VESPER_VERSION = "0.1.0";
+
+#include <fstream>
 #include <iostream>
+#include <sstream>
 
 #include "interpreter.hpp"
 #include "lexer.hpp"
 #include "parser.hpp"
 
-int main() {
-  std::string source = "let x = 10;"
-                       "let y = x + 20;"
-                       "print(x);"
-                       "print(y);"
-                       "print(x * y);";
+int main(int argc, char* argv[])
+{
+    if (argc != 2)
+    {
+        std::cerr << "Usage: vesper <file.vsp>\n"
+                  << "       vesper --help\n"
+                  << "       vesper --version\n";
+        return 1;
+    }
 
-  Lexer lexer(source);
-  auto tokens = lexer.tokenize();
+    std::string argument = argv[1];
 
-  Parser parser(tokens);
-  Program program = parser.parseProgram();
+    if (argument == "--help" || argument == "-h")
+    {
+        std::cout
+            << "Vesper " << VESPER_VERSION << '\n'
+            << '\n'
+            << "Usage:\n"
+            << "  vesper <file.vsp>    Run a Vesper program\n"
+            << "  vesper --help        Show this help message\n"
+            << "  vesper --version     Show version information\n";
 
-  Interpreter interpreter;
-  interpreter.execute(program);
+        return 0;
+    }
 
-  return 0;
+    if (argument == "--version" || argument == "-v")
+    {
+        std::cout << "Vesper " << VESPER_VERSION << '\n';
+        return 0;
+    }
+
+    std::ifstream file(argument);
+
+    if (!file)
+    {
+        std::cerr << "Could not open file: "
+                  << argument << '\n';
+        return 1;
+    }
+
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+
+    std::string source = buffer.str();
+
+    Lexer lexer(source);
+    auto tokens = lexer.tokenize();
+
+    Parser parser(tokens);
+    Program program = parser.parseProgram();
+
+    Interpreter interpreter;
+    interpreter.execute(program);
+
+    return 0;
 }
