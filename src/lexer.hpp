41545926fd532e9389349eb22828a14798a35c71
Tajul Tonim <tajul.islam.tonim@gmail.com>
2026-09-18 09:00:01 +0600

    robot.txt added

--- src/lexer.hpp
+++ src/lexer.hpp
@@ -9,10 +9,6 @@ class Lexer {
 
 public:
   Lexer(const std::string &source);
-
-  char current() const;
-  char peek() const;
-  void advance();
   std::vector<Token> tokenize();
 
 private:
@@ -20,6 +16,9 @@ private:
   std::size_t position = 0;
   int line = 1;
   int column = 1;
+  char current() const;
+  char peek() const;
+  void advance();
   Token readNumber();
   Token readString();
   Token readComment();
